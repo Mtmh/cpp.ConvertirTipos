25c7aa476aad614db0d02298288d464a737a90b2
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-27 11:59:51 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r;
+    int a, b;
+
+    a = 5, b = 2;
+    r = float(a) / float(b);
+
+    cout << r << endl;
+
+    return 0;
+}
